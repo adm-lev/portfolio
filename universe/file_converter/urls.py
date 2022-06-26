cc3e16ede300aba073acd8db4636355a1a1ef37a
Eugene <rheingoldgalaxy@gmail.com>
2022-06-26 22:51:57 +0300

    ocr works
    Распознавание загруженного изображения

--- universe/file_converter/urls.py
+++ universe/file_converter/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,3 +9,8 @@ urlpatterns = [
     re_path(r'^convert/$', views.load_files, name='convert'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
--- universe/file_converter/views.py
+++ universe/file_converter/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from .models import OperationFile
 from django.views import generic
 from django.core.files.storage import FileSystemStorage
 import os
@@ -8,8 +7,11 @@ import pytesseract
 from pdf2image import convert_from_path
 import fitz
 import docx
+from django.conf import settings
 
 
+
+MEDIA = r'/home/devlev/portfolio/universe/media'
 
 def index(request):
     """
@@ -38,16 +40,18 @@ def load_files(request):
             pages[0].save('out.jpg', 'JPEG')
 
             imgcv = cv2.imread(request.FILES['my_file1'])
+
             imgcv = cv2.cvtColor(imgcv, cv2.COLOR_BGR2RGB)
 
             res_text = pytesseract.image_to_string(imgcv)
 
-        elif name.endswith('jpg'):
-            img = cv2.imread(str(file_url))
-            # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        elif name.endswith('jpeg'):
+            # img = cv2.imread('/home/devlev/portfolio/universe/media/converter/files/222.jpeg')
+            img = cv2.imread(os.path.join(settings.MEDIA_ROOT, 'converter/files/', file.name))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # cv2.imshow('Result', img)
             # cv2.waitKey(0)
-
+            # print(img)
             res_text = pytesseract.image_to_string(img)
 
         return render(
@@ -63,8 +67,32 @@ def load_files(request):
     return render(request, 'file_converter/load_file.html')
 
 
+def test():
+
+    img = cv2.imread('/home/devlev/portfolio/universe/media/converter/files/111.jpeg')
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # cv2.imshow('Result', img)
+    # cv2.waitKey(0)
+
+    return pytesseract.image_to_string(img)
+
+
+def test2():
+    file = fitz.open('/home/devlev/portfolio/universe/media/converter/files/clients.pdf')
+
+    # doc = docx.Document()
+
+    for pageNum, page in enumerate(file.pages(), start=1):
+        text = page.get_text()
+        print(text)
+        # doc.add_paragraph(text)
+        # doc.save('test.docx')
+        if pageNum == 1:
+            break
+
+        return text
 
 
 
-
+# test2()
 
