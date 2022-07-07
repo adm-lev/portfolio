3b77bb6e57669b14f74a07b42cabc88d3557e837
Eugene <rheingoldgalaxy@gmail.com>
2022-07-07 17:03:11 +0300

    markup/css 6

--- universe/exchange_rates/tests.py
+++ universe/exchange_rates/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from requests import get, utils
+from decimal import Decimal
+from datetime import date
+import re
 
-# Create your tests here.
+
+# def currency_rates(name):
+
+response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+encodings = utils.get_encoding_from_headers(response.headers)
+content = response.content.decode(encoding=encodings)
+
+# ind = content.find(name.upper())
+# if ind == -1:
+#     return None
+
+cur_date = content[content.find('ValCur')+14:content.find('ValCur')+24]
+# current_content = content[ind:ind + 150]
+valutes = {}
+res = content.split('><Valute')
+for valute in res:
+    print(valute)
+# print(content)
+
+
+
--- universe/seabattle/tests.py
+++ universe/seabattle/tests.py
@@ -1,3 +1,17 @@
+
+
 from django.test import TestCase
 
-# Create your tests here.
+
+def add_num(path):
+    import os
+    with open(path) as file:
+        with open(path + '11', 'a') as file2:
+            for num, line in enumerate(file):
+                file2.write(str(num + 1) + ' ' + line)
+    os.rename(path + '11', path)
+
+
+
+add_num('sample.txt')
+
